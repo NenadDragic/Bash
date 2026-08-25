0b1fef6043cd8e6a4dad84edfaf703a3cce89013
Nenad Ahlstrøm Dragic <nad@edora.hq>
2026-08-25 09:35:56 +0200

    Move GliNet scripts to Devices/Router/GliNet (history preserved via subtree)

--- GliNet/Old/File-Delete-FTP-Upload.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sh /root/Scripts/Delete_10Days_Old_Files.sh  > /root/File-Delete/"$(date '+%Y%m%d_%H%M%S').txt"
-
-sshpass -f /root/Adm/pw.txt rsync -avz --ignore-existing /root/File-Delete SFTP@nas.dragic.com:/volume1/Ftp/DashCam
